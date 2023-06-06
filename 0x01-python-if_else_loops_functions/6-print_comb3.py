f7da91d29b98de6cc0e1b9966d92a3c8116221a2
ayoub zakaria <ayoub.don.zakaria@gmail.com>
2023-06-06 13:31:14 -0700

    hey Brain

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for num1 in range(0, 10):
     for num2 in range(num1+1, 10):
         if num1 == 8 and num2 == 9:
-            print("{}{}".format(num1, num))
+            print("{}{}".format(num1, num2))
         else:
-        print("{}{}".format(num1, num2), end=", ")
+            print("{}{}".format(num1, num2), end=", ")
