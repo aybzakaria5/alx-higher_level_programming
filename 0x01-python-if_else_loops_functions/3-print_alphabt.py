6762b4e1a59bab1b232a937ab33eebe2165a7d04
ayoub zakaria <ayoub.don.zakaria@gmail.com>
2023-06-06 12:35:01 -0700

    printing alphabets except e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ltr in range(97, 123):
-    if chr(ltr) is not 'e'and chr(ltr) is not 'e':
+    if chr(ltr) is not 'e'and chr(ltr) is not 'q':
         print("{}".format(chr(ltr)), end="")
