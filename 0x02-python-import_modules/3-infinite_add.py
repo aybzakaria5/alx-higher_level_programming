ae6c88b19381a2d71d5887ff10c46af40861a4b8
Ayoub Zakarya <85744427+aybzakaria5@users.noreply.github.com>
2023-06-08 18:52:53 +0000

    infint cals

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     sum0 = 0
     n = len(sys.argv)
     for i in range(n - 1):
-         sum0 += int(sys.argv[i + 1])
+        sum0 += int(sys.argv[i + 1])
     print("{}".format(sum0))
