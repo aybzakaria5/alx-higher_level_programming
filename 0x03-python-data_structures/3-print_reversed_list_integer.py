073ee804c79fc9d5422ddfdd5aed8033eac2d945
Ayoub Zakarya <85744427+aybzakaria5@users.noreply.github.com>
2023-06-12 13:22:52 +0000

    printing a list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        """using Slicing OPerator , we can use the reverse() methode"""
+        rev = my_list[::-1][i]
+        print("{:d}".format(rev))
