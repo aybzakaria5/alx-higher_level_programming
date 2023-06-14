7d5745e84736d200966bf2f57e84c5b0fbb59df5
Ayoub Zakarya <85744427+aybzakaria5@users.noreply.github.com>
2023-06-14 23:23:35 +0000

    a dic * 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dictionary = a_dictionary.copy()
-    for i in list(new_dictionary.key()):
-        new_dictionary[i] *= 2
-    return(new_dictionary)
+    for key in list(new_dictionary.keys()):
+        new_dictionary[key] *= 2
+    return new_dictionary
