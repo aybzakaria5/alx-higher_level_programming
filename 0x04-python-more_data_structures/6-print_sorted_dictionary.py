acbd2ebcb958108f579be3854e3f5494071faa94
Ayoub Zakarya <85744427+aybzakaria5@users.noreply.github.com>
2023-06-14 23:06:56 +0000

    print sorted dic

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,11 @@
-#!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    list_ord = list(a_dictionary.keys())
-    list_ord.sort()
-    for i in list_ord:
-        print("{}: {}".format(i, a_dictionary.get(i)))
+    keys = list(a_dictionary.keys())
+    n = len(keys)
+
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if keys[j] > keys[j + 1]:
+                keys[j], keys[j + 1] = keys[j + 1], keys[j]
+
+    for key in keys:
+        print("{}: {}".format(key, a_dictionary[key]))
