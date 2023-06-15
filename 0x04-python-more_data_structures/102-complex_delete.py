517225b8bbbd4a71e8f611712fc1e63a9ebd903f
Ayoub Zakarya <85744427+aybzakaria5@users.noreply.github.com>
2023-06-15 00:28:06 +0000

    del

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    ls_keys = list(a_dictionary.keys())
+    for i in ls_keys:
+        if value == a_dictionary.get(i):
+            del a_dictionary[]
+        return a_dictionary
