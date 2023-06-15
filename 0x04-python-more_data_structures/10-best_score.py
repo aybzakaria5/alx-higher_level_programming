b84d670377126729dd7d120759675295a2220891
Ayoub Zakarya <85744427+aybzakaria5@users.noreply.github.com>
2023-06-15 00:04:56 +0000

    max of a dic

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(a_dictionary, key=a_dictionary.get)
+        max_key = None
+        max_value = 0
+
+        for key, value in a_dictionary.items():
+            if value > max_value:
+                max_key = key
+                max_value = value
+
+        return max_key
+
     return None
