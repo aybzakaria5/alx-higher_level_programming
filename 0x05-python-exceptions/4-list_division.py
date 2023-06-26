fd5f7bb70901321e484696ee1344aa6eb5f56c38
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-06-27 00:17:48 +0100

    lsiting a division and handling errors

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,4 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             result[i] = 0
+        finally:
+            pass
     return result
