cbde24db5727f7d8713005629a8fd301006b78e9
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-06-27 00:22:53 +0100

    lsiting a division and handling errors

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-"""or  we can use result.append() at finally"""
 def list_division(my_list_1, my_list_2, list_length):
     result = [0] * list_length
     for i in range(list_length):
@@ -17,3 +16,4 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             pass
     return result
+    """we can use result.append in finally"""
