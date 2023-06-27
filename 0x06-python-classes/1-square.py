0f57d7d66a4e6320b2931d633a7165a76cae3c20
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-06-27 23:21:55 +0100

    fix error2 for task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,12 +6,8 @@ difingnig a square based on 0-square.py
 
 
 class Square:
-    """
-    defining a class square
-    """
+    """defining a class square"""
 
     def __init__(self, size):
         self.__size = size
-        """
-        instance of the classe with private size attribute        
-        """
+        """instance of the classe with private size attribute"""
