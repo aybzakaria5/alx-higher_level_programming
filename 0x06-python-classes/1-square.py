dbb0c2202c6ecef5ca79e8df83b75026257f0c2f
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-06-27 23:25:32 +0100

    fixing docstring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,23 @@
-#!/usr/bin/python
-"""
-This is a module that write a class Square 
-difingnig a square based on 0-square.py
-"""
+#!/usr/bin/python3
+"""Square class file"""
 
 
 class Square:
-    """defining a class square"""
+    """
+    class Square that defines a square
 
+    Attributes:
+        __size: size of a side of the square
+    """
+
+    # initialisation of an object size
     def __init__(self, size):
+        """
+        Init a square
+
+        Args:
+            size (int): size of the square
+
+        Returns: None
+        """
         self.__size = size
-        """instance of the classe with private size attribute"""
