812d9486edac37d19b4ebff22ba662afdb515b37
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-06-27 01:18:18 +0100

    executing a function handling errors

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(*args)
+        output = fct(*args)
         return output
     except Exception as error:
         sys.stderr.write("Exception: {}\n".format(error))
