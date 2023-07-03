188cbcc61bda4c57531c50484334ab8f59773bd6
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-07-03 18:40:51 +0100

    more about OOP

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" this a file about an empty class"""
+
+
+class Reactangle:
+"""empty class about an rectangle"""
+
+    pass
---
+++ 0x08-python-more_classes/RAEDME.md
@@ -0,0 +1,1 @@
+this is a readme about classes and methods ,  atrinutes ...OOP
