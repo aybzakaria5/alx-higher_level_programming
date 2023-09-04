ed2d339279b6c60f22ca10f939d45facb32c227f
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-04 21:19:19 +0100

    adding new blank line

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -15,10 +15,12 @@ class Rectangle:
             ValueError: if it's less than
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
         Rectangle.number_of_instances += 1
+
     @property
     def width(self):
         """gets the widht of a rectangle
