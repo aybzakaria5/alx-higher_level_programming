a30811f84661782e23a9a3803e680b1639726e4e
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-04 16:36:18 +0100

    addint the width and height instance

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""This module defines a Rectangle with it's operactions"""
+
+
+class Rectangle:
+    """
+        a class that define a rectangle:
+
+        Args:
+            width(int): int
+            weight(int): int
+
+        Raises:
+            TypeError: not an integer
+            ValueError: if it's less than
+    """
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """gets the widht of a rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value: int):
+        """setting the width of the rectangle"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """gets the height of a rectangle"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value: int):
+        """sets the height of the ractangle"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
