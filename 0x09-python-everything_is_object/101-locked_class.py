e57493ee2edf7872bd427b7c6f7cf7d038ccc2c7
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-06 23:16:31 +0100

    using the static type methode slots

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+""" a locked clss that prevents the usuer
+    from dynamically creating an instance
+"""
+
+
+class LockedClass:
+    """preventing the user from creating an
+    instance  
+    """
+    __slots__ = ['first_name']
