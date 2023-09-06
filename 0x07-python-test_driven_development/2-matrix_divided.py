24102064925b82c59cad8548dc8187d22a0cd078
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-06 19:02:57 +0100

    fixing  the opcodestyle

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,20 +15,24 @@ def matrix_divided(matrix, div):
     """
 
     div_matrix = [[]]
+    type_error = "matrix must be a matrix (list of lists) of integers/floats"
+    size_error = "Each row of the matrix must have the same size"
+    div_num = "div must be a number"
+    div_zero = "division by zero"
     if not all(
         isinstance(matrix, list) and
         all(isinstance(elem, (int, float)) for elem in row)
         for row in matrix
     ):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(type_error)
 
     for row in matrix:
         if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError(size_error)
     if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
+        raise TypeError(div_num)
     elif div == 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError(div_zero)
     else:
         div_matrix = [[round(elem / div, 2) for elem in row] for row in matrix]
 
