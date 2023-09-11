ed6147c54b8af8a8abd081ca5afae40cddb335d1
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-11 20:22:03 +0100

    task 0 and using the dir() funciton

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a fcuntion that retursn a list  """
+
+
+def lookup(obj):
+    """a function that retursn a list of
+        of attributes and modules of and obj
+    """
+
+    return dir(obj)
