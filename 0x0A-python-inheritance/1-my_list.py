98bae6c47e49cdaf75508764becad231166b1906
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-11 21:37:40 +0100

    fixing some checks

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,5 @@ class MyList(list):
     """a class that inherets the list"""
     def print_sorted(self):
         """printing the sorted list"""
-        n = len(self)
-        for i in range(n-1):
-            for j in range(0, n-i-1):
-                if self[j] > self[j+1]:
-                    self[j], self[j+1] = self[j+1], self[j]
-        return self
 
+        print(sorted(self))
