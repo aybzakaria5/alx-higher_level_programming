2be1b16abd5f97f31c64018d8171b339fbf183a4
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-12 02:07:58 +0100

    adding a methde to the class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a class about geometry"""
+
+
+class BaseGeometry:
+    def area(self):
+        """a funciton that checks the area"""
+
+        raise Exception("area() is not implemented")
