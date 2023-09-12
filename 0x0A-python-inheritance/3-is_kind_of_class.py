21cc49a83cdeb34c60c60da5cb37387462ffaf32
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-12 01:47:09 +0100

    chacks if the object is an inhereted instanve

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ a fucntion that returns
+    if the ovjct is an inxtance/object of a class
+    """
+
+    return (isinstance(obj, a_class))
