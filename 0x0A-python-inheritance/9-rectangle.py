d26ad3d031b709ea7b1449580ad80cdb00d0a993
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-12 02:27:17 +0100

    adding the str and repr to the class

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""currently empty class"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        """the constructer
+
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """area of the rectangle
+
+        Returns:
+            rectangle's area
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """rectnagle
+
+        Returns:
+            info
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def __repr__(self):
+        """represent the rectangle"""
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
+
