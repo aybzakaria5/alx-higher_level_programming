f5ea83e777b26af3ca9073ec6b66048bc5484a5d
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-13 12:26:33 +0100

    reading from a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""reading a file"""
+
+
+def read_file(filename=""):
+    """
+        a fucntion that reads a file
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
+        f.closed
