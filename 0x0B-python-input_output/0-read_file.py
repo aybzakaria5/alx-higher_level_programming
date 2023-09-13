31f1a4948c98c90047e73f9c5e4e010e528c577e
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-13 12:28:23 +0100

    fixing pycd and some checks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
         a fucntion that reads a file
     """
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
         f.closed
