5b14415fb7a1c6f24534e19928953e9e4f7e2e81
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-13 21:12:12 +0100

    loading from jsin file to an objct

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''module to create a an object from json file 
+'''
+
+
+import json
+
+
+def load_from_json_file(filename):
+    ''' objct to json file
+    '''
+    with open(filename, 'r', encoding="utf-8") as f:
+        return (json.loads(f.read()))
