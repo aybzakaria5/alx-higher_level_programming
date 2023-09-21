84180448e80c83b417cbfad53db515ac608831ef
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-22 00:34:01 +0100

    fixing pycodestyle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -118,7 +118,7 @@ class Rectangle(Base):
         for i in range(self.y):
             print()
         for i in range(self.height):
-            print( " " * self.x + "#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         return (
