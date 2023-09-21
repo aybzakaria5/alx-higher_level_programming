b0f3b45f0265c02a2ec7e76340e59a13cf03fc95
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-21 16:13:49 +0100

    adding the test for base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -12,11 +12,11 @@ class TestBase(unittest.TestCase):
     def test_ids(self):
         b1 = Base()
         b2 = Base()
-        b3 = Base(25)
+        b3 = Base(5)
         b4 = Base()
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 25)
+        self.assertEqual(b3.id, 5)
         self.assertEqual(b4.id, 3)
 
 
