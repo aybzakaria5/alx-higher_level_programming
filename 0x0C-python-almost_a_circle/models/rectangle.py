dc4564488dd1dbaefd1dbbc067e09f7d6fbfa896
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-22 14:50:19 +0100

    adding the args func

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -125,3 +125,20 @@ class Rectangle(Base):
             f"[Rectangle] ({self.id}) "
             f"{self.x}/{self.y} - {self.width}/{self.height}"
         )
+
+    def update(self, *args):
+
+        """
+            assging each arg to each attr
+        """
+        if args and len(args):
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
