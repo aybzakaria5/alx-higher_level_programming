675c862dc1313dbf1a00522121809d40c1e2bbd4
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-22 20:46:49 +0100

    adding the json serialization methode

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 """A BAse Module"""
+import json
 
 
 class Base:
@@ -14,3 +15,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """a methode that serialize a data using json"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
