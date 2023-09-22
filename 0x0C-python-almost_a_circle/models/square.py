4d68262f231575ce9069b7520e2ccd2592c4230c
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-22 16:39:52 +0100

    adding the square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""the square module"""
+
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """THE SQUARE MODULE THAT INHEREITS FROM RECTANGLE"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """the constructor for the square class
+
+        Args:
+            size ([int]): [the size of the square which is width/height]
+            x (int, optional): [the position x]. Defaults to 0.
+            y (int, optional): [the position y]. Defaults to 0.
+            id ([int], optional): [the id]. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+        def __str__(self):
+            return (
+                f"[square] ({self.id}) "
+                f"{self.x}/{self.y} - {self.height}"
+            )
