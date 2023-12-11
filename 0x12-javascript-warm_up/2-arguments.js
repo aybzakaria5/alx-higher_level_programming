1eabdd06c4b9a37c96f56ec69502f6e13b6bde33
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-11 21:03:06 +0100

    task2 v2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,10 +4,8 @@ const [argv, len] = [process.argv, process.argv.length];
 
 if (len === 2) {
   console.log('No argument');
-} else {
-  if (len === 3) {
+} else if (len === 3) {
     console.log('Argument found');
   } else { 
     console.log('Arguments found');
-  }
 }
