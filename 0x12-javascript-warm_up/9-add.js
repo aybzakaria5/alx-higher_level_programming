c5fd142c720ca5f457b7ec4d0a3cefd0ccaa8940
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-11 22:28:46 +0100

    adding task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+function add(a, b) {
+  return parseInt(a) + parseInt(b);
+}
+const result = add(arg1, arg2);
+
+console.log(result);
