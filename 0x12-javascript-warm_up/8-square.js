df49c9a1642ff82783783dd3ddd3d82cf4dea6f2
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-11 22:20:06 +0100

    adding task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const arg = process.argv[2];
+// const Print = 'X'
+
+if (!isNaN(arg)) {
+    const Size = parseInt(arg);
+
+    for (i = 0; i < Size; i++) {
+        console.log('X'.repeat(Size));
+    }
+} else{
+    console.log('Missing size');
+}
