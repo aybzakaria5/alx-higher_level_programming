4296813dce72bd127d8578e403d8d88c68a84964
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-11 23:16:07 +0100

    adding task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  const numbers = args.map(Number);
+  const sortedNumbers = numbers.sort((a, b) => b - a);
+  console.log(sortedNumbers[1]);
+}
