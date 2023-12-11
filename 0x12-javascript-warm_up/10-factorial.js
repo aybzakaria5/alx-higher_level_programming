5fa80f27c74ce7b5dc18a2118813be2e90b7770d
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-11 22:50:58 +0100

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const arg = process.argv[2]
+
+function fct (num) {
+    if (num === 0) {
+        return 1;
+    }
+    return num * fct((num - 1))
+}
+
+if (!isNaN(arg)) {
+    const num = parseInt(arg)
+    console.log(fct(num))
+} else {
+    console.log(fct(0))
+}
