9f0e51d3205fcea96a1f7e4ed285145732af4308
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-11 23:19:37 +0100

    adding task 12

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,1 +1,9 @@
+guillaume@ubuntu:~/0x12$ cat 12-object.js
 #!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
