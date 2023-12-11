fe18e11e6050139df8e1ea0425a9b90ddb2047f9
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-11 21:47:34 +0100

    adding task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const [, , arg] = process.argv;
+const num = parseInt(arg, 10);
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log('Not a number');
+}
