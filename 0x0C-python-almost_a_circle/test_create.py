187d1cbb00b16667b3620336a98d7dd901873992
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-11 21:09:30 +0100

    task2 v2

---
+++ 0x0C-python-almost_a_circle/test_create.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 17-main """
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(3, 5, 1)
+    r1_dictionary = r1.to_dictionary()
+    print(r1_dictionary)
+    print(r1)
