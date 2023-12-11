2cb0e20a5becd558cd668333f7896de6662d305d
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-11 22:01:19 +0100

    adding task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const arg = process.argv[2];
+
+if (!isNaN(arg) && parseInt(arg) > 0) {
+  const x = parseInt(arg);
+
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurrences");
+}
