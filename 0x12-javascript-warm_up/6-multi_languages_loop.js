79ccfc0597749c3bd379c0aa77be86182902fca1
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-11 21:57:48 +0100

    task6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-for (let i = 0; i < lines.length; i++) {
-    console.log(lines[i])
+let i = 0;
+
+while (i < lines.length) {
+  console.log(lines[i]);
+  i++;
 }
