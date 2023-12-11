c50490ae76f925ab0911ce3c627b0d52be5bf695
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-11 20:56:57 +0100

    fixes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
+
 const [argv, len] = [process.argv, process.argv.length];
+
 if (len === 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
-    if (len === 3) {
-        console.log('Argument found');
-    } else { 
-        console.log('Arguments found');
-    }
+  if (len === 3) {
+    console.log('Argument found');
+  } else { 
+    console.log('Arguments found');
+  }
 }
