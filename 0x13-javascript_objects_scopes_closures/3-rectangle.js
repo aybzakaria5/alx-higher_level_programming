74a129cdadb1141578edd2ef0e8311a770d5be2c
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-12 20:11:55 +0100

    adding the print methode -> task3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 & h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.create(null);
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
