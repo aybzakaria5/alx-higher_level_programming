46883d0abd8149a0f9a55abc806c30c732c59f66
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-12 20:36:42 +0100

    fixes for task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 
-class Suqare extends Rectangle {
+class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
 }
-module.exports = Suqare;
+module.exports = Square;
