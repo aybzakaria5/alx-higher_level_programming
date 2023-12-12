c7117bf1b442e69e248399d2172f3704e5ab3b13
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-12 23:18:18 +0100

    adding task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+  const len = list.length;
+  const mid = Math.floor(len / 2);
+
+  for (let i = 0; i < mid; i++) {
+    const tmp = list[i];
+    list[i] = list[len - 1 - i];
+    list[len - 1 - i] = tmp;
+  }
+  return list;
+};
+// we can use the unshift() methode => it adds the elemnt at the
+// begining of the new list
+// or using the push() methode => it adds the element at the end
