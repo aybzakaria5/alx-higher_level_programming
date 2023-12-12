2707e2e72cd7004cc6ba96b94078c480047411bc
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-12 19:58:02 +0100

    adding task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 & h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.create(null);
+    }
+  }
+}
+module.exports = Rectangle;
