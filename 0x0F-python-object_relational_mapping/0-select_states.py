f8af77f3aca92ecf9febf847a16dc636f40a7a55
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-15 16:43:41 +0100

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,21 +10,20 @@ if __name__ == "__main__":
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
-    
+
     # Create a cursor
     cursor = db.cursor()
-    
+
     # Define the query
     query = "SELECT * FROM states ORDER BY id ASC"
-    
+
     # Execute the query
     cursor.execute(query)
-    
+
     # Print the rows
     for row in cursor.fetchall():
         print(row)
-    
+
     # Close the cursor and the database connection
     cursor.close()
     db.close()
-    
