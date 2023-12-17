0ebc3724d9a917066fd289a90bb6bf3e7689d879
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-12-18 00:34:19 +0100

    modelstate

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""creating tables and link it to the DB"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ a class inherting from Base that creates a table
+    with columns and links to the MySql db"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
