52f9339cbb367373a1459f727570cdd9cb5ff2e2
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-01 16:37:20 +0100

    scraping movie titles from an api

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// a script that uses an api to scrap some data from it
+const request = require('request');
+const filmId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + filmId;
+if (process.argv.length !== 3) {
+    console.log("Usage: <./file.js> <filmeId>")
+    process.exit(1)
+}
+
+request.get(url, (err, res, body) => {
+  if (res) {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  } else {
+    console.log(err);
+  }
+});
