59bb32970101711824c9e4730fa28b85b046354e
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-01 16:04:32 +0100

    fixing

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 // a script that retrivtes the satatu code of a request
 
-const request = require("request");
+const request = require('request');
 
 const url = process.argv[2];
-if (process.argv !== 2) {
-    console.log("Usage:  <./file.js> <url>")
-    process.exit(1)
+if (process.argv.length !== 3) {
+  console.log('Usage:  <./file.js> <url>');
+  process.exit(1);
 }
 
 request.get(url, (error, response) => {
-    if (response) {
-        console.log('code: ', response.statusCode);
-    }
-    else {
-        console.log(error)
-    }
+  if (response) {
+    console.log('code: ', response.statusCode);
+  } else {
+    console.log(error);
+  }
 });
