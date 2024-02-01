28a7a315c724979b4188a973add20cb26bda134a
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-01 15:58:16 +0100

    getting the status code from a request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// a script that retrivtes the satatu code of a request
+
+const request = require("request");
+
+const url = process.argv[2];
+if (process.argv !== 2) {
+    console.log("Usage:  <./file.js> <url>")
+    process.exit(1)
+}
+
+request.get(url, (error, response) => {
+    if (response) {
+        console.log('code: ', response.statusCode);
+    }
+    else {
+        console.log(error)
+    }
+});
