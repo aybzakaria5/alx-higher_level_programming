497be8d22c66530ea034af92dd60fd1ed30463fe
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-01 14:14:04 +0100

    updating the script that reads from a file => task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 // a script that reads from a file
-const { error } = require('console');
 const fs = require('fs');
 const pathFile = process.argv[2];
 
 fs.readFile(pathFile, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
