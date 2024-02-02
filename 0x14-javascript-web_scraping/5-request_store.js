4c9f4f66914a91471c784992620f492ecb6257b7
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-02 14:59:54 +0100

    task 5 : scraping and writing into a file
    scraping an urk given as the seconde arg and file name
    as the third arg , then scraping its body
    using request , then writing it in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// a script that scrape a url and write its body into a file
+
+const request = require('request');
+const fs = require('fs');
+const URL = process.argv[2];
+const fileName = process.argv[3];
+if (process.argv.length !== 3) {
+  console.log('Usage : <./script.js> <url> <filename>');
+  process.exit(1);
+}
+request.get(URL, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(fileName, body, { encoding: 'utf-8', flag: 'w' }, (erro) => {
+      if (err) {
+        console.error(erro);
+      }
+    });
+  }
+});
