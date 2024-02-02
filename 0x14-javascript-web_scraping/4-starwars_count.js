871ae12d07ce0164b7406ddc36373f6464da981a
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-02 14:33:19 +0100

    updating

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,17 +3,25 @@
 // and gets inforamtion from api
 // getting in how many movies a person acted
 // using the api
-const request = require('request');
+// const request = require('request');
 const url = process.argv[2];
-const actor = 'https://swapi-api.alx-tools.com/api/people/18/';
-request(url, { json: true }, (err, res, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    //   const count = body.split('/people/18/');
-    const films = body.results;
-    const match = films.filter((film) => film.characters.includes(actor));
-    const count = match.length;
-    console.log(count);
+// const actor = 'https://swapi-api.alx-tools.com/api/people/10/';
+// request(url, { json: true }, (err, res, body) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     //   const count = body.split('/people/18/');
+//     const films = body.results;
+//     const match = films.filter((film) => film.characters.includes(actor));
+//     const count = match.length;
+//     console.log(count);
+//   }
+// });
+const request = require('request');
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
   }
+  console.log(body.split('/people/18/').length - 1);
 });
