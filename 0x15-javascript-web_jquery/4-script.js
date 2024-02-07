1f49f31d8fa49a2deb868f2046ded2a154c88d4c
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-08 00:53:50 +0100

    task 4

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,1 +1,4 @@
-#!/usr/bin/node
+$('DIV#toggle_header').click(function () {
+  $('header').toggleClass('red green');
+});
+// When the user clicks on DIV#toggle_header, the class of the HTML tag HEADER must be toggled between red and green. If the current class is red, the class must be updated to green, and vice-versa.
