56313093c0e3a9092e9e9d8d7f3265eadb215227
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-08 01:39:52 +0100

    adding new item using append and query selectors

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+$(document).ready(function () {
+  $('#add_item').click(function () {
+    $('ul.my_list').append('<li></li>');
+  });
+});
+// When the user clicks on DIV#add_item, a new element LI is added to the list UL.my_list.
