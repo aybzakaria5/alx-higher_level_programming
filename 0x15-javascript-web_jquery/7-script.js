659e07c7ddf798ab8339d763ceee3db7e8b51007
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-12 17:17:26 +0100

    detching from an api and manipulating the html

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+$.ajax({
+    url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+    method: 'GET',
+    datatype: 'json',
+    success: (data) => {
+        $('DIV#character').text(data.name)
+        // $(br).addClass('height');
+        // $('.height').text(data.height);
+        // this two lines above just to test if the height is being printed
+        console.log(data.height);
+    }
+});
