aaa107031fa4d536a894d0c0d8eaad38757cecdf
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-12 18:02:06 +0100

    task 9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+$.ajax({
+    url: "https://hellosalut.stefanbohacek.dev/?lang=fr",
+    method: "GET",
+    dataType: "json",
+    success: (data) => {
+        $("DIV#hello").text(data.hello)
+    }
+});
